bbda698fc50fed7a25d255f306d2888e6c06ad4e
josehenriqueroveda <josehenriqueroveda.dev@gmail.com>
2025-01-23 14:04:32 -0300

    feat: add `purchase-requisition` page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,8 @@
 import "./globals.css";
+import { Bar } from "@ui5/webcomponents-react";
+import { Button } from "@ui5/webcomponents-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata = {
   title: "UI5 PRPO",
@@ -8,8 +12,43 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en-us">
+      <body>
+        <div>
+          <Bar
+            design="Header"
+            endContent={
+              <span>
+                <Button design="Negative">
+                  <Link href="/">Logout</Link>
+                </Button>
+              </span>
+            }
+            startContent={
+              <span>
+                <Image
+                  alt="SAP Logo"
+                  width={48}
+                  height={48}
+                  src="https://sap.github.io/ui5-webcomponents/images/sap-logo-svg.svg"
+                />
+              </span>
+            }
+          >
+            <span>
+              <Button design="Default">
+                <Link href="/purchase-requisition">Purchase Requisiton</Link>
+              </Button>
+            </span>
+            <span>
+              <Button design="Default">
+                <Link href="/">Purchase Order</Link>
+              </Button>
+            </span>
+          </Bar>
+        </div>
+        <div>{children}</div>
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import { Text } from "@ui5/webcomponents-react";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -9,7 +11,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Developed by: Jose Henrique Roveda
+          <Text>Developed by: Jose Henrique Roveda</Text>
         </a>
       </footer>
     </div>
